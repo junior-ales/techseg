3a15beab08fd287e94d2a54140d3fa785f3bfb50
Junior Ales <edilson.ales.jr@gmail.com>
2014-01-24 11:52:45 -0200

    Configuracoes semelhantes ao passo-a-passo do site do codeship.io

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
- 
-app.get('/', function(request, response) {
-  response.sendfile(__dirname + '/app/index.html');
-}).configure(function() {
-  app.use(express.static(__dirname + '/app'));
-  app.use(express.static(__dirname + '/bower_components'));
-}).listen(port);
+var app = function () {
+  var http = require('http');
+  var express = require('express');
+  var logfmt = require("logfmt");
+  var app = express();
+
+  app.set('views', __dirname + '/views');
+  app.engine('html', require('ejs').renderFile);
+  app.get('/', function(req, res) {
+    res.render('index.html');
+  });
+  app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/bower_components'));
+    app.use(logfmt.requestLogger());
+  })
+
+  return app;
+}();
+
+module.exports = app;
+
