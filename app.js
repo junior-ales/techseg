3a09136a7c8147882815f167a7b73551652d3f86
Junior Ales <edilson.ales.jr@gmail.com>
2014-01-24 03:05:51 -0200

    Configurando arquivos para o heroku

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-
-app.use(express.static(__dirname + '/app'));
-app.use(express.static(__dirname + '/bower_components'));
-
-app.listen(port);
+ 
+app.get('/', function(request, response) {
+  response.sendfile(__dirname + '/app/index.html');
+}).configure(function() {
+  app.use(express.static(__dirname + '/app'));
+  app.use(express.static(__dirname + '/bower_components'));
+}).listen(port);
