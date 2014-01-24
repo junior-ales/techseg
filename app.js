494b021113679fcaf139b2c5e6a1ffeacddae82c
Junior Ales <edilson.ales.jr@gmail.com>
2014-01-24 02:24:17 -0200

    Adicionando suporte do expressjs

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/bower_components'));
+
+app.listen(port);
