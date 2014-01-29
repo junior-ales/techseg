671bb798d2408083f5188a1bc2c21fa321abff2c
Junior Ales <edilson.ales.jr@gmail.com>
2014-01-29 04:38:21 -0200

    Arrumando bug com localConfig

--- start.js
+++ start.js
@@ -1,7 +1,17 @@
 var app = require("./app");
-var localConfig = require('./localConfig');
+
+var localKey = function() {
+  try {
+    return require('./localConfig').mandrillKey;
+  } catch(e) {
+    console.error('Problema com configuração local. Criou o localConfig.js na raiz do projeto? Mais info no README');
+    process.exit(e.code);
+  }
+}
+
 var port = process.env.PORT || 3000;
-var key = process.env.MANDRILL_KEY || localConfig.mandrillKey;
+var key = process.env.MANDRILL_KEY || localKey();
+
 
 app.set('MANDRILL_KEY', key);
 app.listen(port)
