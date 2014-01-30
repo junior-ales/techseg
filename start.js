e6ac70793b9e7347855c82607ffd399f6538910d
Junior Ales <edilson.ales.jr@gmail.com>
2014-01-30 02:51:59 -0200

    Arrumando o bug no carregamento do email do destinatario

--- start.js
+++ start.js
@@ -1,19 +1,21 @@
-var app = require("./app");
+var app = require('./app');
 
-var localConfig = function() {
+var key, email;
+if (process.env.MANDRILL_KEY && process.env.EMAIL_RECEIVER) {
+  key = process.env.MANDRILL_KEY;
+  email = process.env.EMAIL_RECEIVER;
+} else {
   try {
-    return require('./localConfig');
+    var localConfig = require('./localConfig');
+    key = localConfig.mandrillKey;
+    email = localConfig.emailReceiver;
   } catch(e) {
     console.error('Problema com configuração local e/ou variaveis de ambiente não foram criadas corretamente. Em ambiente de desenvolvimento, verifique se o localConfig.js foi criado na raiz do projeto. Mais informações no README');
     process.exit(e.code);
   }
-}();
-
-var port = process.env.PORT || 3000;
-var key = process.env.MANDRILL_KEY || localConfig.mandrillKey;
-var email = process.env.EMAIL_RECEIVER || localConfig.emailReceiver;
+}
 
 app.set('MANDRILL_KEY', key);
 app.set('EMAIL_RECEIVER', email);
-app.listen(port);
+app.listen(process.env.PORT || 3000);
 
